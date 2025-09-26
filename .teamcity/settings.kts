5563f4074e057a4bf2fd6e15e447b4c77585af30
ym <ym@TeamCity>
2025-09-26 12:49:33 +0000

    general settings of 'Build' build configuration were updated (TeamCity change in 'Netology' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -35,7 +35,7 @@ project {
 object Build : BuildType({
     name = "Build"
 
-    artifactRules = "target/*.jar"
+    artifactRules = "target/*.jar => target"
 
     vcs {
         root(DslContext.settingsRoot)
